d0fd4affda01c3a5ff23aa2490fdd9237aec8a66
Eriel Mbonihankuye <erielandy673@gmail.com>
2020-07-22 18:13:22 +0200

    intial commit

--- index.php
+++ index.php
@@ -13,3 +13,29 @@ echo "</br>La somme de ".$a. " est celle de ".$b." est ".$c;
 $d= $c/5;
 
 echo "</br>le resultat de la division de " .$c. " par 5 est ".$d;
+
+//les boolens
+
+if($a != $b){
+    echo "</br> ".$a. " et ".$b." sont differents";
+}elseif ($a < $b) {
+    # code...
+    echo $a." est superieur a".$b;
+}else {
+    
+    echo $b." est superieur a ".$a;
+}
+
+//les tableaux
+$cars = array("toyota", "nissan", "lexus", "mercedes");
+var_dump($cars);
+
+function multiplication($e,$f){
+
+    $c=$e * $f;
+
+    return $c;
+}
+
+echo multiplication(12,24);
+
