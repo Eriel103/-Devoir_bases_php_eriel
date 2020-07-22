caa0e621e8b769a12c8aaeab1fd11748d9c0672d
Eriel Mbonihankuye <erielandy673@gmail.com>
2020-07-22 17:07:36 +0200

    ajout des oerateurs arithmetique

--- index.php
+++ index.php
@@ -5,3 +5,11 @@ $b=14;
 
 echo "LA valeur de a est ".$a. " est celle de b est ".$b;
 
+//Les operateurs arithmetique
+
+$c= $a+$b;
+echo "</br>La somme de ".$a. " est celle de ".$b." est ".$c;
+
+$d= $c/5;
+
+echo "</br>le resultat de la division de " .$c. " par 5 est ".$d;
